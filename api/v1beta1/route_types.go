826dea51cfb77c249a7659739ca2ea62414c8789
Cedric Poon <cedric@poons.me>
2020-09-09 17:42:51 +0800

    operator-sdk init with spec.type=loadbalancer

---
+++ api/v1beta1/route_types.go
@@ -0,0 +1,196 @@
+/*
+Copyright 2020 cedio.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+// RouteSpec defines the desired state of Route
+type RouteSpec struct {
+	// Name of the Service which this route is backed
+	ServiceName string `json:"serviceName" protobuf:"bytes,1,opt,name=serviceName"`
+
+	// Route type for the route to operate
+	// Supported: ingress/loadbalancer
+	Type RouteType `json:"type" protobuf:"bytes,2,opt,name=type,casttype=RouteType"`
+
+	// If routeType="loadbalancer", configuration of loadbalancer will be applied
+	Loadbalancer *LoadbalancerConfig `json:"loadbalancer,omitempty" protobuf:"bytes,3,opt,name=loadbalancer"`
+
+	// If routeType="loadbalancer", configuration of loadbalancer will be applied
+	Ingress *IngressConfig `json:"ingress,omitempty" protobuf:"bytes,4,opt,name=ingress"`
+}
+
+// RouteType indicates the type of route it supports
+type RouteType string
+
+const (
+	// RouteTypeIngress indicates voyager ingress is used for routing
+	RouteTypeIngress RouteType = "ingress"
+
+	// RouteTypeLoadbalancer indicates metallb is used for routing
+	RouteTypeLoadbalancer RouteType = "loadbalancer"
+)
+
+// LoadbalancerConfig defines config for route backed using metallb
+type LoadbalancerConfig struct {
+	// Loadbalancer IP Pool for Service
+	// Supported: internal/external
+	AddressPool LBAddressPoolType `json:"addressPool" protobuf:"bytes,1,opt,name=addressPool,casttype=LBAddressPoolType"`
+
+	// Specific IP for metallb to assign to Service
+	TargetIP string `json:"targetIP,omitempty" protobuf:"bytes,2,opt,name=targetIP"`
+}
+
+// LBAddressPoolType indicates the IP Pool for metallb to route
+type LBAddressPoolType string
+
+const (
+	// LBAddressPoolInternal indicates internal as ips for metallb
+	LBAddressPoolInternal LBAddressPoolType = "internal"
+
+	// LBAddressPoolExternal indicates external as ips for metallb
+	LBAddressPoolExternal LBAddressPoolType = "external"
+)
+
+// IngressConfig defines config for route backed using Voyager ingress
+type IngressConfig struct {
+	// Mode of Voyager ingress backend
+	// Supported: tcp/http
+	Backend IngressBackendType `json:"backend" protobuf:"bytes,1,opt,name=backend,casttype=IngressBackendType"`
+
+	// Hostname of Ingress typed Route
+	// Automatically generated as <service-name>-<namespace-name>.<cluster-domain> if not supplied
+	// +optional
+	Host string `json:"host,omitempty" protobuf:"bytes,2,opt,name=host"`
+
+	// Port for http or tcp, optional for http
+	// +optional
+	Port *int `json:"port,omitempty" protobuf:"varint,3,opt,name=port"`
+
+	// The target port on pods selected by the service this route points to.
+	// If this is a string, it will be looked up as a named port in the target
+	// endpoints port list.
+	ServicePort intstr.IntOrString `json:"servicePort" protobuf:"bytes,4,opt,name=servicePort"`
+
+	// TLS configuration for Ingress
+	// +optional
+	TLS *IngressTLSConfig `json:"tls,omitempty" protobuf:"bytes,5,opt,name=tls"`
+}
+
+// IngressTLSConfig defines the TLS encryption method for ingress typed Route
+type IngressTLSConfig struct {
+	// Name of Secret for SSL certificate used to perform TLS encryption
+	SecretName string `json:"secretName" protobuf:"bytes,1,opt,name=secretName"`
+}
+
+// IngressBackendType indicates which ingress backend is used
+type IngressBackendType string
+
+const (
+	// IngressBackendHTTP shows http is used
+	IngressBackendHTTP IngressBackendType = "http"
+
+	// IngressBackendTCP shows tcp is used
+	IngressBackendTCP IngressBackendType = "tcp"
+)
+
+// RouteStatus defines the observed state of Route
+type RouteStatus struct {
+	// Ingress status of Route as list
+	Ingress []RouteIngress `json:"ingress,omitempty" protobuf:"bytes,1,rep,name=ingress"`
+
+	// Loadbalancer status of Route as list
+	Loadbalancer []RouteLoadbalancer `json:"loadbalancer,omitempty" protobuf:"bytes,2,rep,name=loadbalancer"`
+}
+
+// RouteIngress holds information about the places where a route is exposed as ingress
+type RouteIngress struct {
+	// Host is the host string under which the route is exposed; this value is required
+	Host string `json:"host,omitempty" protobuf:"bytes,1,opt,name=host"`
+
+	// Port for http or tcp, optional for http
+	// +optional
+	Port *int `json:"port,omitempty" protobuf:"varint,2,opt,name=port"`
+
+	// Conditions is the state of the route, may be empty.
+	Conditions []RouteCondition `json:"conditions,omitempty" protobuf:"bytes,3,rep,name=conditions"`
+}
+
+// RouteLoadbalancer holds information about the places where a route is exposed as lb
+type RouteLoadbalancer struct {
+	// IP is the location which the route is exposed as loadbalancer
+	IP string `json:"ip,omitempty" protobuf:"bytes,1,opt,name=ip"`
+
+	// Conditions is the state of the route, may be empty.
+	Conditions []RouteCondition `json:"conditions,omitempty" protobuf:"bytes,2,rep,name=conditions"`
+}
+
+// RouteCondition contains details for the current condition of this route on a particular router
+// ref: OpenShift API Route
+type RouteCondition struct {
+	// Type is the type of the condition.
+	// Currently only Ready.
+	Type RouteConditionType `json:"type" protobuf:"bytes,1,opt,name=type,casttype=RouteConditionType"`
+	// Status is the status of the condition.
+	// Can be True, False, Unknown.
+	Status corev1.ConditionStatus `json:"status" protobuf:"bytes,2,opt,name=status,casttype=k8s.io/api/core/v1.ConditionStatus"`
+	// (brief) reason for the condition's last transition, and is usually a machine and human
+	// readable constant
+	Reason string `json:"reason,omitempty" protobuf:"bytes,3,opt,name=reason"`
+	// Human readable message indicating details about last transition.
+	Message string `json:"message,omitempty" protobuf:"bytes,4,opt,name=message"`
+	// RFC 3339 date and time when this condition last transitioned
+	LastTransitionTime *metav1.Time `json:"lastTransitionTime,omitempty" protobuf:"bytes,5,opt,name=lastTransitionTime"`
+}
+
+// RouteConditionType is a valid value for RouteCondition
+type RouteConditionType string
+
+// These are valid conditions of pod.
+const (
+	// RouteAdmitted means the route is able to service requests for the provided Host
+	RouteAdmitted RouteConditionType = "Admitted"
+)
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// Route is the Schema for the routes API
+type Route struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   RouteSpec   `json:"spec,omitempty"`
+	Status RouteStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// RouteList contains a list of Route
+type RouteList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Route `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Route{}, &RouteList{})
+}
---
+++ controllers/route_controller.go
@@ -0,0 +1,132 @@
+/*
+Copyright 2020 cedio.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	routerv1beta1 "v8s-router/api/v1beta1"
+)
+
+// RouteReconciler reconciles a Route object
+type RouteReconciler struct {
+	client.Client
+	Log    logr.Logger
+	Scheme *runtime.Scheme
+}
+
+const (
+	addressPoolAnnotationField  = "metallb.universe.tf/address-pool"
+	serviceLastManagedTimeField = "router.v8s.cedio.dev/last-managed-time"
+	serviceRouteTypeField       = "router.v8s.cedio.dev/type"
+)
+
+// +kubebuilder:rbac:groups=router.v8s.cedio.dev,resources=routes,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=router.v8s.cedio.dev,resources=routes/status,verbs=get;update;patch
+
+func (r *RouteReconciler) patchServiceLoadBalancer(ro *routerv1beta1.Route, service *corev1.Service) error {
+	annotations := map[string]string{
+		serviceLastManagedTimeField: time.Now().Format(time.RFC3339),
+		addressPoolAnnotationField:  string(ro.Spec.Loadbalancer.AddressPool),
+		serviceRouteTypeField:       string(ro.Spec.Type),
+	}
+	service.ObjectMeta.Annotations = annotations
+	service.Spec.Type = corev1.ServiceTypeLoadBalancer
+	if ro.Spec.Loadbalancer.TargetIP != "" {
+		service.Spec.LoadBalancerIP = ro.Spec.Loadbalancer.TargetIP
+	}
+
+	return nil
+}
+
+func (r *RouteReconciler) manageService(ro *routerv1beta1.Route, reqLogger logr.Logger) (*ctrl.Result, error) {
+	service := &corev1.Service{}
+	err := r.Get(context.TODO(), types.NamespacedName{Name: ro.Spec.ServiceName, Namespace: ro.Namespace}, service)
+	if err != nil {
+		reqLogger.Error(err, "Failed to get Route Service")
+		return &reconcile.Result{}, err
+	}
+	if ro.Spec.Loadbalancer == nil {
+		reqLogger.Error(err, "Error getting spec.loadbalancer")
+		return &reconcile.Result{}, errors.NewBadRequest("Missing spec.loadbalancer for spec.type='loadbalancer'")
+	}
+	err = r.patchServiceLoadBalancer(ro, service)
+	if err != nil {
+		reqLogger.Error(err, "error getting server service")
+		return &reconcile.Result{}, err
+	}
+	err = r.Update(context.TODO(), service)
+	if err != nil {
+		reqLogger.Error(err, "Failed to patch Route Service", "Service.Namespace", service.Namespace, "Service.Name", service.Name)
+		return &reconcile.Result{}, err
+	}
+	return nil, nil
+}
+
+func (r *RouteReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
+	_ = context.Background()
+	reqLogger := r.Log.WithValues("route", req.NamespacedName)
+
+	// Fetch instance
+	ro := &routerv1beta1.Route{}
+	err := r.Get(context.TODO(), req.NamespacedName, ro)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	// Determine Spec.Type
+	switch ro.Spec.Type {
+	case routerv1beta1.RouteTypeIngress:
+
+	case routerv1beta1.RouteTypeLoadbalancer:
+		// Patch related Service to regarding type
+		reconcileResult, err := r.manageService(ro, reqLogger)
+		if err != nil {
+			ro.Status.Loadbalancer = nil
+			if err := r.Status().Update(context.TODO(), ro); err != nil {
+				reqLogger.Error(err, "Failed to update Route Status")
+			}
+			return *reconcileResult, err
+		} else if err == nil && reconcileResult != nil {
+			// In case requeue required
+			return *reconcileResult, nil
+		}
+	default:
+		return ctrl.Result{}, errors.NewBadRequest("Supported spec.type: ['ingress', 'loadbalancer']")
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func (r *RouteReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&routerv1beta1.Route{}).
+		Complete(r)
+}
